9f8131f7bfbe9c07855004c9a431b6b894b18bf4
Heegyeong <fhskf94kr@naver.com>
2018-08-17 18:57:13 +0900

    9786410 fhskf94kr 2163 맞았습니다!! 11388 KB 112 ms Java/ 수정 271 B 1분 전

---
+++ 2163/2163_YHG.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Problem_2163 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		
+		int n = scn.nextInt(); 
+		int m = scn.nextInt(); 
+		
+		System.out.println(n*m-1);
+	}
+}
