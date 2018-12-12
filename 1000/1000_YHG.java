7875a201617ee0f58766387014444e8a27d7047d
Heegyeong <fhskf94kr@naver.com>
2018-12-12 21:32:34 +0900

    11021033 fhskf94kr 1000 맞았습니다!! 14356 108 Java / 수정 265

---
+++ 1000/1000_YHG.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Problem_1000 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+
+		int A = scn.nextInt();
+		int B = scn.nextInt();
+		
+		System.out.println(A+B);
+	}
+}
