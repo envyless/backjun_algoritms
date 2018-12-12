bfd78be26a631faf8dff002e06ac9cfaefcc6f20
Heegyeong <fhskf94kr@naver.com>
2018-12-12 21:33:31 +0900

    11021141 fhskf94kr 1008 맞았습니다!! 15996 116 Java / 수정 277

---
+++ 1008/1008_YHG.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Problem_1008 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+
+		double A = scn.nextDouble();
+		double B = scn.nextDouble();
+		
+		System.out.printf("%.9f", A/B);
+	}
+}
